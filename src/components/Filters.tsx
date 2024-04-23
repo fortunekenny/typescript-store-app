73e78c3bc5267c1089eeeb937d1bed6170b38fe7
fortunekenny <gbephraim@gmail.com>
2024-04-23 13:46:37 +0100

    product filter component2

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -3,16 +3,19 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
 import { type ProductsResponseWithParams } from "@/utils";
+import { FormInput, FormSelect, FormRange, FormCheckbox } from "@/components";
 
 function Filters() {
     const { meta, params } = useLoaderData() as ProductsResponseWithParams;
-  const { search } = params;
+    const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
-      <div className="mb-2">
-        <Label htmlFor="search">Search Product</Label>
-        <Input id="search" name="search" type="text" defaultValue={search} />
-      </div>
+        <FormInput type="search" label="search product" name="search" defaultValue={search} />
+        <FormSelect name="category" label="select category" defaultValue={category} options={meta.categories}/>
+        <FormSelect name="company" label="select company" defaultValue={company} options={meta.companies}/>
+        <FormSelect name="order" label="order by" defaultValue={order} options={['a-z','z-a','high','low']}/>
+        <FormRange label="price" name="price" defaultValue={price} />
+        <FormCheckbox label="free shipping" name="shipping" defaultValue={shipping} />
       <Button type="submit" size="sm" className="self-end mb-2">
         search
       </Button>
@@ -26,6 +29,7 @@ function Filters() {
         <Link to="/products">reset</Link>
       </Button>
     </Form>
+
   );
 }
 export default Filters;
