35f1261da6e2a647400ce14fba60d67d23a8a5bc
fortunekenny <gbephraim@gmail.com>
2024-04-24 16:53:10 +0100

    pagination

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,4 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "./ui/button";
 import { type ProductsResponseWithParams } from "@/utils";
 import { FormInput, FormSelect, FormRange, FormCheckbox } from "@/components";
@@ -10,11 +8,17 @@ function Filters() {
     const { search, company, category, shipping, order, price } = params;
   return (
     <Form className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+       {/** SERACH */}
         <FormInput type="search" label="search product" name="search" defaultValue={search} />
+       {/** CATEGORIES */}
         <FormSelect name="category" label="select category" defaultValue={category} options={meta.categories}/>
+       {/** COMPANY */}
         <FormSelect name="company" label="select company" defaultValue={company} options={meta.companies}/>
+       {/** ORDER BY */}
         <FormSelect name="order" label="order by" defaultValue={order} options={['a-z','z-a','high','low']}/>
+       {/** RANGE/SLIDER */}
         <FormRange label="price" name="price" defaultValue={price} />
+       {/** CHECKBOX */}
         <FormCheckbox label="free shipping" name="shipping" defaultValue={shipping} />
       <Button type="submit" size="sm" className="self-end mb-2">
         search
